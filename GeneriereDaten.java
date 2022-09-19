ff862435db0333197304eebb4ea599045201c075
Johannes Häusler <mail@spotlightforbugs.eu>
2022-09-19 14:11:41 +0200

    Create GeneriereDaten.java

---
+++ GeneriereDaten.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * Beschreibung
+ *
+ * @version 1.0 vom 19.09.2022
+ * @author johhae
+ */
+
+public class GeneriereDaten extends WetterStation {
+  
+  // Anfang Attribute
+  // Ende Attribute
+  
+  // Anfang Methoden
+  public void GeneriereDaten() {
+    // generiere zufaellige Daten für das Array Datenspeicher[7][24] (Datentyp double)
+    
+    
+  }
+
+  // Ende Methoden
+} // end of GeneriereDaten
