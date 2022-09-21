8640bf29e1ca9427e64a238c083b3a0852c82c0d
SpotlightForBugs <mail@spotlightforbugs.eu>
2022-09-21 16:27:53 +0200

    first layout

---
+++ weatherHub.java
@@ -0,0 +1,14 @@
+public class weatherHub {
+
+    public static void receiveData(String[][] dataString) {
+    }
+
+    public static void receiveData(double[][][][] data, String name, String location) {
+    }
+
+    public static void setupWeatherStation(String name, String location) {
+    }
+    
+  
+    
+}
